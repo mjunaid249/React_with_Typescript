8f4a59a58120c923ba6e4cc7f5d6beea8c5157fc
mjunaid249 <junaidch249249@gmail.com>
2024-02-19 19:23:29 +0500

    TS

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,39 @@
 import { useState } from "react";
 import "./App.css";
-import Box from "./components/Box";
+
+interface Person {
+  name: string;
+  age: number;
+}
 
 function App() {
-  const [val, setVal] = useState<string>("");
+  const [user, setUser] = useState<Person>();
+
+  const submitHandler = () => {
+    console.log(user);
+  };
 
   return (
     <>
-      <Box label="Search" value={val} setter={setVal} />
+      <form onSubmit={submitHandler}>
+        <input
+          type="number"
+          placeholder="Age"
+          value={user?.age}
+          onChange={(e) =>
+            setUser((prev) => ({ ...prev!, age: Number(e.target.value) }))
+          }
+        />
+        <input
+          type="text"
+          value={user?.name}
+          placeholder="Name"
+          onChange={(e) =>
+            setUser((prev) => ({ ...prev!, name: String(e.target.value) }))
+          }
+        />
+        <button type="submit">Register</button>
+      </form>
     </>
   );
 }
